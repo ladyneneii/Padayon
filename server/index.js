ef5059b86362663a02c98fe5312ab350ea5ff9fa
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-11-12 12:10:13 +0800

    Created and connected to database

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 app.use(cors());
+const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
@@ -25,14 +28,48 @@ io.on("connection", (socket) => {
   socket.on("send_message", (data) => {
     console.log(data);
     // .to(data.room) means only the users in the same room can receive the message
-    socket.to(data.room).emit("receive_message", data)
-  })
+    socket.to(data.room).emit("receive_message", data);
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected", socket.id);
   });
 });
 
-server.listen(3001, () => {
+server.listen(port, () => {
   console.log("SERVER RUNNING");
+});
+
+app.use(express.static("../client")); // This directs it to the index.html file in the client folder
+
+// bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// MySQL
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "padayon",
+});
+
+// Get all users
+app.get("/api/users", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+
+    // query(sqlString, callback)
+    connection.query("SELECT * FROM users", (err, rows) => {
+      connection.release(); // return the connection to pool
+
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+    });
+  });
 });
