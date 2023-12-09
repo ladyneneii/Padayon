131c150a6594aa806202946ba890b5a7eab6fbdd
DESKTOP-RBOQA39\pc user <ernestcurativo@gmail.com>
2023-12-10 00:24:01 +0800

    Added Post component

--- server/index.js
+++ server/index.js
@@ -442,3 +442,37 @@ app.get("/api/locations", (req, res) => {
     });
   });
 });
+
+// Add a post
+app.post("/api/posts", upload.single("avatar_url"), (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    const params = req.body;
+
+    connection.query("INSERT INTO posts SET ?", params, (err, rows) => {
+      connection.release(); // return the connection to pool
+
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    connection.query("SELECT * FROM posts", (err, rows) => {
+      connection.release(); // return the connection to pool
+
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
